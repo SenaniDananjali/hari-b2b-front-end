3e84ad0ce352cb675f5e4875d8188b1c501f52c5
senanidananjali <senanidananjali@gmail.com>
2017-12-15 15:46:04 +0530

    calendar update

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -11,18 +11,47 @@ export class CalendarComponent implements OnInit {
 
 
   public today;
-  public todayName;
+  public day1;
+  public day2;
+  public day3;
+  public day4;
+  public day5;
+  public day6;
+  public day7;
+  public day8;
+  public day9;
+  public day10;
+  public day11;
+  public day12;
+  public day13;
+  public day14;
+  public day15;
 
-  public dateArray: [''];
 
   constructor() {
   }
 
   ngOnInit() {
 
-    this.today = this.getDate(0).format('d');
-    this.todayName = this.getDate(0).format('dddd');
-    this.setdateArray();
+    this.today = this.getDate(0);
+    this.day1 = this.getDate(1);
+    this.day2 = this.getDate(2);
+    this.day3 = this.getDate(3);
+    this.day4 = this.getDate(4);
+    this.day5 = this.getDate(5);
+    this.day6 = this.getDate(6);
+    this.day7 = this.getDate(7);
+    this.day8 = this.getDate(8);
+    this.day9 = this.getDate(9);
+    this.day10 = this.getDate(10);
+    this.day11 = this.getDate(11);
+    this.day12 = this.getDate(12);
+    this.day13 = this.getDate(13);
+    this.day14 = this.getDate(14);
+
+
+    //this.todayName = this.getDate(0).format('dddd');
+    //this.setdateArray();
     console.log(this.getDate(4));
     //this.dateArray.push() this.getDate(4);
   }
@@ -32,34 +61,5 @@ export class CalendarComponent implements OnInit {
 
   }
 
-
-  setdateArray() {
-
-    let num: number;
-    for (num = 0; num < 14; num++) {
-      //this.dateArray [num] = this.getDate(num);
-      //  dateArray.push(this.getDate(num));
-    }
-    return this.dateArray;
-  }
-
-
-  /*
-    createCalendar(month) {
-      const moment = require('moment');
-      const firstday = moment(month).startOf('M');
-      const days = Array.apply(null, {length: month.daysInMonth()})
-        .map(Number.call, Number);
-      console.log(moment().weekday(-7));
-
-      console.log(moment());
-
-      console.log(moment(new Date()).add(6,'days'));
-      console.log(moment(new Date()).add(7,'days').format('dddd'));
-
-
-
-
-  */
 
 }
