e24d4f84d963767607d163c8d88e3e18cec1f284
senanidananjali <senanidananjali@gmail.com>
2017-12-15 19:09:43 +0530

    calendar update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
@@ -30,6 +31,7 @@ const appRoutes: Routes = [
     FooterComponent,
     StylistComponent,
     CalendarComponent,
+
 
 
   ],
