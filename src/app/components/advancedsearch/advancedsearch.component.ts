57b15978b0d17925dd0e25c0c361c30ecf7a637f
senanidananjali <senanidananjali@gmail.com>
2017-12-21 15:03:12 +0530

    -Advanced Search

---
+++ src/app/components/advancedsearch/advancedsearch.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-advancedsearch',
+  templateUrl: './advancedsearch.component.html',
+  styleUrls: ['./advancedsearch.component.css']
+})
+export class AdvancedsearchComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit() {
+
+  }
+
+}
